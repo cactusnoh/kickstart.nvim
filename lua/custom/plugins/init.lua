20cbef9daafffa1e469b07a0a10bdb667dc27e5a
cactusnoh <cactusnoh@gmail.com>
2025-05-06 19:03:20 +0900

    Add keymap for copying the absolute path of current buffer

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.keymap.set('n', '<leader>cp', function()
+  vim.fn.setreg('+', vim.api.nvim_buf_get_name(0))
+end, { desc = 'Copy absolute path of current file' })
+
 return {}
